7b476dc82ff878e398ce7335adff827dfb94385b
hfma2000 <hfma2000@qq.com>
2017-05-07 15:10:56 +0800

    show weather info...

--- app/src/main/java/cn/czmec/myweather/util/Utility.java
+++ app/src/main/java/cn/czmec/myweather/util/Utility.java
@@ -77,7 +77,7 @@ public class Utility {
     public static Weather handleWeatherResponse(String response) {
         try {
             JSONObject jsonObject = new JSONObject(response);
-            JSONArray jsonArray = jsonObject.getJSONArray("HeWeathe");
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
             String weatherContent = jsonArray.getJSONObject(0).toString();
             return new Gson().fromJson(weatherContent,Weather.class);
         } catch (JSONException e) {
