2d15a888f3d20fad7ed87a799f09e523c402cc12
hfma2000 <hfma2000@qq.com>
2017-05-08 08:48:10 +0800

    statusbar透明

--- app/src/main/java/cn/czmec/myweather/WeatherActivity.java
+++ app/src/main/java/cn/czmec/myweather/WeatherActivity.java
@@ -47,7 +47,7 @@ public class WeatherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         if (Build.VERSION.SDK_INT >= 21) {
             View decorView = getWindow().getDecorView();
-            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
